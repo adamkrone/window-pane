750468f3a40783991423163583954ffd00458f80
Adam Krone <krone.adam@gmail.com>
2013-10-09 15:57:46 -0500

    Adds basic slideshow functionality

--- windowPane.js
+++ windowPane.js
@@ -17,8 +17,17 @@
 				windowPane: "",
 				insertInto: "",
 				image: "",
+				imageType: "single",
 				bgRepeat: "no-repeat",
-				revealRollovers: false
+				revealRollovers: false,
+				slideshowSpeed: 5000,
+				slideSpeed: 1000,
+				autoAdvance: false,
+				slideDirection: "next",
+				slideStyle: "single",
+				includeNav: false,
+				navPrev: ".nav-prev",
+				navNext: ".nav-next"
 			};
 
 			defaults.insertInto = options.windowPane;
@@ -29,57 +38,138 @@
  * Variables
  *****************************************************************************/
 
-			var WP = {},
-				i = 0;
+			var WP = {};
 
 			WP.self = $(this);
 			WP.image = new Image();
 			WP.position = [];
+			WP.animating = false;
+
+			WP.slide = function (direction) {
+				if (WP.animating === false) {
+					var windowElements = $(options.insertInto, WP.self);
+
+					WP.stopSlideshow();
+					WP.animating = true;
+
+					windowElements.each(function (i, windowElement) {
+						var windowElement = $(windowElement),
+								slideWidth = $(".slide", windowElement).css("width"),
+								firstSlide = $(".slide:first", windowElement),
+								lastSlide = $(".slide:last", windowElement),
+								marginLeft;
+
+						if (direction === "next") {
+							marginLeft = "-=" + slideWidth;
+						} else if (direction == "prev") {
+							marginLeft = "+=" + slideWidth;
+							firstSlide.before(lastSlide);
+							windowElement.css("margin-left", "-=" + slideWidth);
+						}
+
+						windowElement.animate({
+							"margin-left": marginLeft
+						}, options.slideSpeed, function () {
+							if (direction === "next") {
+								lastSlide.after(firstSlide);
+								windowElement.css("margin-left", "+=" + slideWidth);
+							}
+						});
+					});
+
+					setTimeout(function () {
+						WP.animating = false;
+						if(options.autoAdvance) {
+							WP.startSlideshow();
+						}
+					}, options.slideSpeed);
+				}
+			};
+
+			WP.startSlideshow = function () {
+				WP.slideshow = setInterval(function () {
+					WP.slide(options.slideDirection);
+				}, options.slideshowSpeed);
+			};
+
+			WP.stopSlideshow = function () {
+				clearInterval(WP.slideshow);
+			};
 
 /******************************************************************************
  * Setup
  *****************************************************************************/
 
-			//Get top/left position of all "windows"
+			// Get top/left position of all "windows"
 			$(options.windowPane, WP.self).each(function () {
 				WP.position.push($(this).position());
 			});
 
-			//Set offsets
+			// Set offsets
 			WP.offsetTop = WP.position[0].top;
 			WP.offsetLeft = WP.position[0].left;
 
-			//Adjust all "windows" to zero out the position
+			// Adjust all "windows" to zero out the position
 			$.each(WP.position, function () {
 				this.top -= WP.offsetTop;
 				this.left -= WP.offsetLeft;
 			});
 
-			//Insert the image as the background to all targeted elements
-			$(options.insertInto, WP.self).each(function () {
-				$(this).css({
-					"background-image": "url(" + options.image + ")",
-					"background-repeat": options.bgRepeat,
-					"background-position": "-" + WP.position[i].left + "px -" + WP.position[i].top + "px"
+			// Insert image(s)
+			if (options.imageType === "single") {
+				$(options.insertInto, WP.self).each(function (i) {
+					$(this).css({
+						"width": "100%",
+						"height": "100%",
+						"background-image": "url(" + options.image + ")",
+						"background-repeat": options.bgRepeat,
+						"background-position": "-" + WP.position[i].left + "px -" +
+							WP.position[i].top + "px"
+					});
+					i++;
 				});
-				i++;
-			});
+			} else if (options.imageType === "slideshow") {
+				$(options.insertInto, WP.self).each(function (i) {
+					var windowElement = $(this),
+							windowElementWidth = windowElement.css("width");
 
-			//Reveal rollover state if image doesn't cover the background
+					$.each(options.image, function (j) {
+						var slide = $('<div class="slide"></div>');
+
+						windowElement.append(slide);
+
+						slide.css({
+							"float": "left",
+							"width": windowElement.css("width"),
+							"height": windowElement.css("height"),
+							"background-image": "url(" + options.image[j] + ")",
+							"background-repeat": options.bgRepeat,
+							"background-position": "-" + WP.position[i].left + "px -" +
+								WP.position[i].top + "px"
+						});
+					});
+
+					windowElement.css("width",
+						windowElementWidth.substring(0, windowElementWidth.length - 2) *
+							options.image.length);
+				});
+			}
+
+			// Reveal rollover state if image doesn't cover the background
 			if (options.revealRollovers) {
 
-				//Show all previously hidden elements
+				// Show all previously hidden elements
 				$(options.insertInto).show();
 
-				//Setup image height
+				// Setup image height
 				WP.image.src = options.image;
 				WP.image.onload = function () {
 					var i = 0,
 					imageHeight = WP.image.height;
 
-					//Find all window elements below image height
+					// Find all window elements below image height
 					$(options.windowPane, WP.self).each(function () {
-						//Hide elements not covered by image height
+						// Hide elements not covered by image height
 						if (WP.position[i].top >= imageHeight) {
 							$(this).children(options.insertInto).hide();
 						}
@@ -87,6 +177,26 @@
 					});
 				};
 
+			}
+
+			// Start slideshow if autoAdvance is enabled
+			if (options.autoAdvance) {
+				WP.startSlideshow();
+			}
+
+			// Setup nav controls if enabled
+			if (options.includeNav) {
+				$(options.navPrev, WP.self).on("click", function (event) {
+					WP.slide("prev");
+
+					event.preventDefault();
+				});
+
+				$(options.navNext, WP.self).on("click", function (event) {
+					WP.slide("next");
+
+					event.preventDefault();
+				});
 			}
 
 		}
